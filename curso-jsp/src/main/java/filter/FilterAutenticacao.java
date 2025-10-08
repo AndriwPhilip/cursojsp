689c004b1dcbcae3c7e439e04cb4429780cdc3b1
andri <andri@SrAlguem>
2025-10-08 10:41:24 -0300

    Criando conexão com banco

--- curso-jsp/src/main/java/filter/FilterAutenticacao.java
+++ curso-jsp/src/main/java/filter/FilterAutenticacao.java
@@ -13,52 +13,79 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import connection.SingleConnectionBanco;
 
-@WebFilter(urlPatterns = {"/principal/*"})/*Intercepta todas as requisiçoes que vierem do projeto ou mapeamento*/
+@WebFilter(urlPatterns = { "/principal/*" }) /* Intercepta todas as requisiçoes que vierem do projeto ou mapeamento */
 public class FilterAutenticacao implements Filter {
-       
-   
-    public FilterAutenticacao() {        
-    }
 
-    /*Encerra os processos quando o servidor é parado */
-    //Ex: Mataria os processos de conexao com o banco
+	private static Connection connection;
+
+	public FilterAutenticacao() {
+	}
+
+	/* Encerra os processos quando o servidor é parado */
+	// Ex: Mataria os processos de conexao com o banco
 	public void destroy() {
-	
+
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	/* Intercepta as requisicoes e as respostas no sistema */
 	// Tudo que fizer no sistema passa por aqui
-	//Ex: Validaçao de autenticaçao , commit e rollback de transações de banco
+	// Ex: Validaçao de autenticaçao , commit e rollback de transações de banco
 	// validar e fazer redirecionamento
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
 
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpSession session = req.getSession();
-		
-		String usuarioLogado = (String) session.getAttribute("usuario");
-		
-		String urlParaAutenticar = req.getServletPath(); //Url que esta sendo acessada
-		
-		//Validar se esta logado senao redireciona parar a tela de login
-		if(usuarioLogado == null &&
-				!urlParaAutenticar.equalsIgnoreCase("/principal/ServletLogin")) {// Não esta logado
+		try {
 			
-			RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url=" + urlParaAutenticar);
-			request.setAttribute("msg", "Por favor realize o login!");
-			redireciona.forward(request, response);
-			return;//Para a execuçao e redireciona para o login		
-		}else {
-			chain.doFilter(request, response);
+			HttpServletRequest req = (HttpServletRequest) request;
+			HttpSession session = req.getSession();
+
+			String usuarioLogado = (String) session.getAttribute("usuario");
+
+			String urlParaAutenticar = req.getServletPath(); // Url que esta sendo acessada
+
+			// Validar se esta logado senao redireciona parar a tela de login
+			if (usuarioLogado == null && !urlParaAutenticar.equalsIgnoreCase("/principal/ServletLogin")) {// Não esta
+																											// logado
+
+				RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url=" + urlParaAutenticar);
+				request.setAttribute("msg", "Por favor realize o login!");
+				redireciona.forward(request, response);
+				return;// Para a execuçao e redireciona para o login
+			} else {
+				chain.doFilter(request, response);
+			}
+			
+			connection.commit(); // Deu tudo certo confirma as alteraçoes no banco
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+
 		}
-		
+
 	}
 
 	/* Inicio os processos ou recursos quando o servidor sobe o projeto */
-	//Ex: Iniciar a conexao com o banco
+	// Ex: Iniciar a conexao com o banco
 	public void init(FilterConfig fConfig) throws ServletException {
-	
+
+		connection = SingleConnectionBanco.getConnection();
+
 	}
 
 }
